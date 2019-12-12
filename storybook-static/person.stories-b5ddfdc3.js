a6e7dd97078cbeeb53c573a8d8dc2592ccfffdaf
beth-panx <beth_panx@hotmail.com>
2019-12-12 14:34:18 -0800

    initial storybook static site subtree commit

---
+++ storybook-static/person.stories-b5ddfdc3.js
@@ -0,0 +1,2 @@
+import{e as o,f as r}from"./index-3a445c6b.js";import{h as e}from"./lit-html-c7682711.js";import"./Graph-3b72116e.js";import"./mgt-mock-provider-3678bf7a.js";const m=()=>e` <mgt-mock-provider></mgt-mock-provider> <mgt-person person-query="me" show-name show-email></mgt-person> `;export default{title:"Mock Provider|mgt-person",component:"mgt-person",decorators:[o,r]};export{m as person};
+//# sourceMappingURL=person.stories-b5ddfdc3.js.map
